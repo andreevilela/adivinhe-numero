237c511b474c160b1c196d267d6c88d08ff8fdbe
andreevilela <andrevibegoogle@hotmail.com>
2021-04-14 23:07:54 -0300

    Versão 1.0

--- src/index.js
+++ src/index.js
@@ -4,14 +4,45 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Jogo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "", numero: ""};
+  }
+
+  sortearNumero() {
+    return Math.floor(Math.random() * 99) + 1;
+  }
+
+  componentDidMount() {
+    this.setState({numero: this.sortearNumero()});
+  }
+
+  atualizaValor(event) {
+    this.setState({value: event.target.value});
+  }
+
+  verifica(event) {
+    const u = this.state.value;
+    const n = this.state.numero;
+
+    if (u == n) alert("Parabéns, acertou!!");
+    else if (u > n) alert("Sua aposta é maior");
+    else alert ("Sua aposta é menor");
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit = {(e) => this.verifica(e)}>
+        <label>
+          Seu palpite:
+          <input type="number" min="1" max="100" value={this.state.value} onChange={(e) => this.atualizaValor(e)}/>
+        </label>
+        <input type="submit" value="Enviar"/>
+      </form>
+    );
+  }
+  
+}
